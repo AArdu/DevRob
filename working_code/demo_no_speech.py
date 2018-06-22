f2bb02bebb91e9a7f00b38271f5dc0c43397d1f7
Bram van Asseldonk <Bram@2xp.nl>
2018-06-22 10:59:59 +0200

    Deleted the FaceDetector class in demo_no_speech.py

--- working_code/demo_no_speech.py
+++ working_code/demo_no_speech.py
@@ -4,6 +4,7 @@ import numpy as np
 import cv2, time, sys, StringIO, json, httplib, wave, pprint
 import random
 import subprocess
+import FaceDetection
 from GazeFollow import GazeNet as GNet
 
 
@@ -48,80 +49,6 @@ class SpeechRecognition(ALModule):
             sys.exit(0)
             return
 
-class FaceDetector:
-    """
-    Class to detect faces in picture.
-    """
-    def __init__(self, img="face.jpg", debug=False):
-    	self.face_cascade = cv2.CascadeClassifier('./all_data/haarcascade_frontalface_default.xml')
-    	self.eye_cascade = cv2.CascadeClassifier('./all_data/haarcascade_eye.xml')
-        self.loadIm(img)
-        if debug:
-            self.drawFaceBoxes()
-            self.drawCenterFaces()
-            self.drawEyeBoxes()
-            self.showIm()
-
-    def detectFaces(self):
-        """
-        Detects faces in an image.
-        Input:
-        img = cv.imread image Image in which you want to detect faces.
-        Output:
-        faces = [(topLeft_x, topLeft_y, width, height)] List of quadrupples that describe a box around the faces that were detected.
-        """
-    	faces = self.face_cascade.detectMultiScale(self.gray, 1.3, 5)
-    	return faces
-
-    def drawFaceBoxes(self):
-    	for (x, y, w, h) in self.detectFaces():
-    		cv2.rectangle(self.img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
-    def detectCenterFaces(self):
-        centers = []
-        height, width, channels = self.img.shape
-        for (x, y, w, h) in self.detectFaces():
-            centers.append(((x + float(w)/2)/width, (y + float(h)/2)/height))
-        print centers
-        return centers
-
-    def drawCenterFaces(self):
-        height, width, channels = self.img.shape
-        for (x, y) in self.detectCenterFaces():
-            cv2.line(self.img, (int(x*width)-5, int(y*height)), (int(x*width)+5, int(y*height)), (255,0,0))
-            cv2.line(self.img, (int(x*width), int(y*height)-5), (int(x*width), int(y*height)+5), (255,0,0))
-
-    def detectEyes(self):
-        """
-        Detects eyes in an image.
-        Input:
-        img = cv.imread image Image in which you want to detect faces.
-        Output:
-        eyes = [(topLeft_x, topLeft_y, width, height)] List of quadrupples that describe a box around the eyes that were detected.
-        """
-    	for (x, y, w, h) in self.detectFaces():
-    		roi_gray = self.gray[y:y+h, x:x+w]
-    		roi_color = self.img[y:y+h, x:x+w]
-    		eyes = self.eye_cascade.detectMultiScale(roi_gray)
-    	return eyes
-
-    def drawEyeBoxes(self):
-    	for (x, y, w, h) in self.detectFaces():
-    		for (ex, ey, ew, eh) in self.detectEyes():
-    			roi_color = self.img[y:y+h, x:x+w]
-    			cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
-
-    def loadIm(self, img):
-        if isinstance(img, str):
-            self.img = cv2.imread(img)
-        else:
-            self.img = img
-        self.gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
-
-    def showIm(self):
-    	cv2.imshow('img', self.img)
-    	cv2.waitKey(0)
-    	cv2.destroyAllWindows()
 
 def areyoumymom(Speecher):
 	tts_p.say("Its'a me, Marvin")
@@ -166,18 +93,13 @@ def center_face(face):
 
 
 def follow_gaze(cam, GazeNet):
-
-    img = get_remote_image(cam)
-    FaceDetector(img, True)
-
-    #e = FaceDetector.detectCenterFaces()
-    #FaceDetector.drawCenterFaces()
-    #FaceDetector.showIm
-
-    # get gaze directions
-    gaze_coords = Gazenet.getGaze(e, img)
-    # use gaze directions to look and point in that direction
-    point_at_gazed(gaze_coords, cam)
+	img = get_remote_image(cam)
+	fd = FaceDetection(img, True)
+	center_of_face = fd.detectCenterFaces()
+	# get gaze directions
+	gaze_coords = GazeNet.getGaze(center_of_face, img)
+	# use gaze directions to look and point in that direction
+	point_at_gazed(gaze_coords, cam)
 
 def point_at_gazed(coords, cam):
     # get the angles needed to look at the pointed object
